d91c20a7a6d3ded8acf37f58290d81569fbe82c5
zonetecde <zonetecde@gmail.com>
2026-06-01 16:55:44 +0200

    init projet maven

---
+++ src/main/java/fr/zonetec/App.java
@@ -0,0 +1,7 @@
+package fr.zonetec;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/fr/zonetec/AppTest.java
@@ -0,0 +1,8 @@
+package fr.zonetec;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
