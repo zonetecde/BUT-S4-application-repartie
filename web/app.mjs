44405d70f8f5810d5df1c134bb78df8026bb4733
zonetecde <zonetecde@gmail.com>
2026-06-02 09:52:10 +0200

    Ajout de l'affichage et de la récupération des velibs sur la carte

--- web/app.mjs
+++ web/app.mjs
@@ -1,10 +1,12 @@
+import { recupererVelibsNancy } from "./velibs.js";
+
 // On commence par récupérer les coordonnées de Nancy via l'API https://adresse.data.gouv.fr/outils/api-doc/adresse
 const url = "https://data.geopf.fr/geocodage/search?q=Nancy&limit=1";
 
 // On fetch la réponse de l'API
 fetch(url)
     .then((response) => response.json())
-    .then((data) => {
+    .then(async (data) => {
         // On récupère les coordonnées de Nancy depuis la réponse de l'API
         const lon = data.features[0].geometry.coordinates[0];
         const lat = data.features[0].geometry.coordinates[1];
@@ -19,4 +21,13 @@ fetch(url)
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         }).addTo(map);
+
+        // On récupère maintenant les informations sur les stations de velibs à Nancy
+        const stations = await recupererVelibsNancy();
+
+        // Ajoute les stations sur la carte
+        stations.forEach((station) => {
+            const marker = L.marker([station.lat, station.lon]).addTo(map);
+            marker.bindPopup(`<b>${station.nom}</b><br>Adresse : ${station.adresse}<br>Vélos disponibles : ${station.velosDisponibles}<br>Places de parking libres : ${station.placesVoitureDisponibles}`);
+        });
     });
