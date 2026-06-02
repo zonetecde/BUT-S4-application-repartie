950a53d12766fca4104d781e0b77a41c848ecd02
Léa Guerin <leaguerin3@etu.univ-lorraine.fr>
2026-06-02 11:20:48 +0200

    changement BD

--- src/main/java/fr/zonetec/InitDataBase.java
+++ src/main/java/fr/zonetec/InitDataBase.java
@@ -18,44 +18,85 @@ public class InitDataBase {
             " CONSTRAINT pk_restaurant PRIMARY KEY (idRestaurant)" +
             ")";
 
+        String tableTable = 
+            "CREATE TABLE Table_Resto (" +
+            " idTable NUMBER(3) GENERATED AS IDENTITY," +
+            " idRestaurant NUMBER(3)," + 
+            " nbPlaces NUMBER(2)," +
+            " CONSTRAINT pk_table PRIMARY KEY (idTable)," +
+            " CONSTRAINT fk_table_restaurant FOREIGN KEY (idRestaurant) REFERENCES Restaurant(idRestaurant)" +
+            ")";
+
         String reservationTable =
             "CREATE TABLE Reservation (" +
-            "  idRestaurant NUMBER(3)," +
-            "  idReservation NUMBER(3) GENERATED AS IDENTITY," +
-            "  nomClient VARCHAR2(50) NOT NULL," +
-            "  prenomClient VARCHAR2(50)," +
-            "  nbConvives NUMBER(2)," +
-            "  numTel VARCHAR2(15)," +
-            "  CONSTRAINT pk_reservation PRIMARY KEY (idRestaurant, idReservation)," +
-            "  CONSTRAINT fk_res_restaurant FOREIGN KEY (idRestaurant) REFERENCES Restaurant(idRestaurant)" +
+            " idRestaurant  NUMBER(3)," +
+            " idReservation NUMBER(3) GENERATED AS IDENTITY," +
+            " nomClient VARCHAR2(50) NOT NULL," +
+            " prenomClient VARCHAR2(50)," +
+            " idTable NUMBER(3)," + 
+            " nbConvives NUMBER(2)," +
+            " numTel VARCHAR2(15)," +
+            " CONSTRAINT pk_reservation PRIMARY KEY (idRestaurant, idReservation)," +
+            " CONSTRAINT fk_res_restaurant FOREIGN KEY (idRestaurant) REFERENCES Restaurant(idRestaurant)," + 
+            " CONSTRAINT fk_res_table FOREIGN KEY (idTable) REFERENCES Table_Resto(idTable)" +
+            ")";
+
+        String commandeTable = 
+            "CREATE TABLE Commande (" +
+            " idCommande NUMBER(3) GENERATED AS IDENTITY," +
+            " dateCom DATE NOT NULL," +
+            " datePaie DATE," +
+            " montant NUMBER(6,2)," +
+            " nbPers NUMBER(2)," + 
+            " idTable NUMBER(3)," + 
+            " CONSTRAINT pk_commande PRIMARY KEY (idCommande)," +
+            " CONSTRAINT fk_com_table FOREIGN KEY (idTable) REFERENCES Table_Resto(idTable)" +
+            ")";
+
+        String platTable = 
+            "CREATE TABLE Plat (" +
+            " idPlat NUMBER(3) GENERATED AS IDENTITY," +
+            " libelle VARCHAR2(100) NOT NULL," +
+            " prixUnit NUMBER(5,2) NOT NULL," + 
+            " qteStockee NUMBER(4)," +
+            " CONSTRAINT pk_plat PRIMARY KEY (idPlat)" +
+            ")";
+   
+        String contientTable = 
+            "CREATE TABLE Contient (" +
+            " idCommande NUMBER(3)," +
+            " idPlat NUMBER(3)," +
+            " quantite NUMBER(3) NOT NULL," +
+            " CONSTRAINT pk_contient PRIMARY KEY (idCommande, idPlat)," +
+            " CONSTRAINT fk_contient_commande FOREIGN KEY (idCommande) REFERENCES Commande(idCommande)," +
+            " CONSTRAINT fk_contient_plat FOREIGN KEY (idPlat) REFERENCES Plat(idPlat)" +
             ")";
 
         try (Statement stmt = connection.createStatement()) {
-            try {
-                stmt.execute(restaurantTable);
-                System.out.println("Table Restaurant créée");
-            } catch (SQLException e) {
-                    // Si on a une exception c'est très certainement car la table a déjà été créée.
-                if (e.getErrorCode() == 955) {
-                    System.out.println("Table Restaurant déjà existante");
-                } else {
-                    throw e;
-                }
-            }
-
-            try {
-                stmt.execute(reservationTable);
-                System.out.println("Table Reservation créée");
-            } catch (SQLException e) {
-                if (e.getErrorCode() == 955) {
-                    // Si on a une exception c'est très certainement car la table a déjà été créée.
-                    System.out.println("Table Reservation déjà existante");
-                } else {
-                    throw e;
-                }
-            }
+            
+            executeTable(stmt, restaurantTable, "Restaurant");
+            executeTable(stmt, tableTable, "Table_Resto");
+            executeTable(stmt, reservationTable, "Reservation");
+            executeTable(stmt, commandeTable, "Commande");
+            executeTable(stmt, platTable, "Plat");
+            executeTable(stmt, contientTable, "Contient");
+            
         } catch (SQLException e) {
             System.err.println("Erreur lors de la création des tables : " + e.getMessage());
+        }
+    }
+
+    public static void executeTable(Statement stmt, String table, String nomTable) throws SQLException {
+        try {
+            stmt.execute(table);
+            System.out.println("Table" + nomTable + " créée");
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 955) {
+                // Si on a une exception c'est très certainement car la table a déjà été créée.
+                System.out.println("Table " + nomTable + " déjà existante");
+            } else {
+                throw e;
+            }
         }
     }
 
@@ -69,7 +110,7 @@ public class InitDataBase {
                 int count = rs.getInt(1);
 
                 if (count == 0) {
-                    System.out.println("Table vide --> Insertion des données");
+                    System.out.println("Table Restaurant vide --> Insertion des données");
 
                     String resto1 = "INSERT INTO Restaurant (nom, adresse, coordonneesX, coordonneesY) VALUES ('Grand Café Foy', '1 Place Stanislas, 54000 Nancy', 6.183181, 48.693452)";
                     String resto2 = "INSERT INTO Restaurant (nom, adresse, coordonneesX, coordonneesY) VALUES ('Le Bouche à Oreille', '42 Rue des Bastions, 54000 Nancy', 6.179320, 48.697010)";
