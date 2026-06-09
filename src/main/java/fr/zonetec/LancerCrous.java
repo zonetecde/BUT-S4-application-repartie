b3c620493bbbb5bc257266bc5263a07f49c01b36
zonetecde <zonetecde@gmail.com>
2026-06-09 15:46:22 +0200

    Revert "tentative de fix pr un probleme de lancement"
    This reverts commit e201fe8ed0fc250cd09189232991204830a9b891.

--- src/main/java/fr/zonetec/LancerCrous.java
+++ src/main/java/fr/zonetec/LancerCrous.java
@@ -10,17 +10,19 @@ public class LancerCrous {
      * Lance et enregistre le service RMI du CROUS.
      */
     public static void main(String[] args) throws RemoteException {
-        // nécessaire sinon on a des problemes de build
-        System.setProperty("java.rmi.server.codebase", "file:target/restaurant-rmi-server.jar");
-
         // Service RMI du Crous
         Crous crous = new Crous();
 
         try {
             ServiceCrous rd = (ServiceCrous) UnicastRemoteObject.exportObject(crous, 0);
 
-            // Crée l'annuaire RMI
-            Registry reg = LocateRegistry.createRegistry(1099);
+            // Crée ou récupère l'annuaire RMI
+            Registry reg;
+            try {
+                reg = LocateRegistry.createRegistry(1099);
+            } catch (RemoteException e) {
+                reg = LocateRegistry.getRegistry("localhost", 1099);
+            }
             reg.rebind("crous", rd);
 
             System.out.println("Service CROUS enregistré dans l'annuaire RMI");
